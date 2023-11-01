ac0ad8e5e4b68a6882cf19ab2348f183858356a3
Avdhut Madan <rohanmadan05@gmail.com>
2023-11-01 19:22:49 +0530

    I have Added Frontend GUI part which was missing

--- FACE RECOGNITION BASED ATTENDANCE MONITORING SYSTEM/main.py
+++ FACE RECOGNITION BASED ATTENDANCE MONITORING SYSTEM/main.py
@@ -362,7 +362,42 @@ window.configure(background='#262523')
 
 #######FRONT END AVDHUT HERE##########
 
+frame1 = tk.Frame(window, bg="#00aeff")
+frame1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
 
+frame2 = tk.Frame(window, bg="#00aeff")
+frame2.place(relx=0.51, rely=0.17, relwidth=0.38, relheight=0.80)
+
+message3 = tk.Label(window, text="Face Recognition Based Attendance System" ,fg="white",bg="#262523" ,width=55 ,height=1,font=('times', 29, ' bold '))
+message3.place(x=10, y=10)
+
+frame3 = tk.Frame(window, bg="#c4c6ce")
+frame3.place(relx=0.52, rely=0.09, relwidth=0.09, relheight=0.07)
+
+frame4 = tk.Frame(window, bg="#c4c6ce")
+frame4.place(relx=0.36, rely=0.09, relwidth=0.16, relheight=0.07)
+
+datef = tk.Label(frame4, text = day+"-"+mont[month]+"-"+year+"  |  ", fg="orange",bg="#262523" ,width=55 ,height=1,font=('times', 22, ' bold '))
+datef.pack(fill='both',expand=1)
+
+clock = tk.Label(frame3,fg="orange",bg="#262523" ,width=55 ,height=1,font=('times', 22, ' bold '))
+clock.pack(fill='both',expand=1)
+tick()
+
+head2 = tk.Label(frame2, text="                       For New Registrations                       ", fg="black",bg="#3ece48" ,font=('times', 17, ' bold ') )
+head2.grid(row=0,column=0)
+
+head1 = tk.Label(frame1, text="                       For Already Registered                       ", fg="black",bg="#3ece48" ,font=('times', 17, ' bold ') )
+head1.place(x=0,y=0)
+
+lbl = tk.Label(frame2, text="Enter ID",width=20  ,height=1  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold ') )
+lbl.place(x=80, y=55)
+
+txt = tk.Entry(frame2,width=32 ,fg="black",font=('times', 15, ' bold '))
+txt.place(x=30, y=88)
+
+lbl2 = tk.Label(frame2, text="Enter Name",width=20  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold '))
+lbl2.place(x=80, y=140)
 
 #######FRONT END AVDHUT HERE##########
 
