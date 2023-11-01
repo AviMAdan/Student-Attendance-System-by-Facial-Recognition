f4c0c1b5b5259baa5f0f6820f6bca7d710383af5
Venom11111 <109681333+Venom11111@users.noreply.github.com>
2023-11-01 21:39:51 +0530

    Update main.py
    Menubar added

--- FACE RECOGNITION BASED ATTENDANCE MONITORING SYSTEM/main.py
+++ FACE RECOGNITION BASED ATTENDANCE MONITORING SYSTEM/main.py
@@ -430,7 +430,12 @@ message.configure(text='Total Registrations till now  : '+str(res))
 
 ##################### MENUBAR Omkar here#################################
 
-
+menubar = tk.Menu(window,relief='ridge')
+filemenu = tk.Menu(menubar,tearoff=0)
+filemenu.add_command(label='Change Password', command = change_pass)
+filemenu.add_command(label='Contact Us', command = contact)
+filemenu.add_command(label='Exit',command = window.destroy)
+menubar.add_cascade(label='Help',font=('times', 29, ' bold '),menu=filemenu)
 
 ################## TREEVIEW ATTENDANCE TABLE ####################
 
